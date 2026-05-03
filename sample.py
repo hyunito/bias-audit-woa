a02adc1303c2280baf1a25f31547fdef671f2006
iMHEOW <rainieraaronabsalon@gmail.com>
2026-05-03 13:51:22 +0800

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,21 @@
+
+test_data = {
+        'age': [
+            25, 132, 45, 28, 50, 35, 22, 40, 115, 38, 
+            29, 31, 42, 27, 33, 48, -5, 36, 19, 89
+        ],
+        'sex': [
+            'Male', 'Female', 'male', 'Female', 'Male', 'rawr', 'Female', 'Male', 'Male', 'FeMAle',
+            'maLe', 'Female', 'Male', 'FEMALE', 'Male', 'Female', 'unknown', 'Male', 'Female', 'Male'
+        ],
+        'race': [
+            'White', 'Black', 'WHITE', 'asian', 'White', 'Black', 'WhITe', 'White', 'Amer', 'White',
+            'BLAck', 'White', 'Asian-Pac-Islander', 'White', 'black', 'White', 'Other', 'White', 'BlACk', 'White'
+        ],
+        'marital-status': [
+            'Never-married', 'Married-civ-spouse', 'DIVORCED', 'Never-married', 'Married-civ-spouse',
+            'Separated', 'Never-married', 'Married-civ-spouse', 'AF', 'Divorced',
+            'civ', 'Never-married', 'Married-civ-spouse', 'Never-married', 'Widowed',
+            'Married-civ-SPOUSE', 'Never-married', 'Divorced', 'Never', 'abseNT'
+        ]
+    }
