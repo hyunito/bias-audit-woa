6af8850cb7834fe86a1ea9e2305dabdfa44fe84c
hyunito <eujengonzales@gmail.com>
2026-06-13 13:55:31 +0800

    Optimizing tracker

--- check.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('data/provenance_metadata.json', encoding='utf-8') as f:
-    data = json.load(f)
-for step in data:
-    total_fav = sum(group['favorable_outcomes'] for group in step['intersectional_demographics'].values())
-    total_unfav = sum(group['unfavorable_outcomes'] for group in step['intersectional_demographics'].values())
-    print(step['transformation_name'], 'Fav=', total_fav, 'Unfav=', total_unfav)
